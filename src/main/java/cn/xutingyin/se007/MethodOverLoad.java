d1168011fe2d6a77bb83251726306d9cd6b2f432
xuty <sunshinexuty@163.com>
2020-05-26 23:00:31 +0800

    feat:java-se-007 -overload

---
+++ src/main/java/cn/xutingyin/se007/MethodOverLoad.java
@@ -0,0 +1,22 @@
+package cn.xutingyin.se007;
+
+/**
+ * @description: 方法重载
+ * @author: Tingyin.Xu
+ * @email : sunshinexuty@163.com
+ * @create: 2020-05-26 22:59
+ **/
+public class MethodOverLoad {
+
+    public void out(int i) {
+        System.out.println(i + 2);
+    }
+
+    public void out(long l) {
+        System.out.println(l + 2);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
