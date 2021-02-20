2ea66a832888f4851a50ca8d76c815a050631775
xuty <xutingyin@jiuaoedu.com>
2021-02-20 17:47:54 +0800

    feat: update comment

--- src/main/java/cn/xutingyin/jvm/HelloHeapOutOfMemory.java
+++ src/main/java/cn/xutingyin/jvm/HelloHeapOutOfMemory.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 class Person {}
 
+/*
+ *
+ * 功能描述：OutOfMemoryError 测试，VM参数设置：
+ *  -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails
+ *
+ * -XX:+HeapDumpOnOutOfMemoryError  会生成dump 文件
+ * @author TingYin.Xu
+ * @date 2021/2/20 17:15
+ *
+ */
 public class HelloHeapOutOfMemory {
 
     public static void main(String[] args) {
